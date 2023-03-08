570086255982b940c7d51e27760eb0e6a08051e7
invsblmad <aizatmadumarova04@gmail.com>
2023-03-08 11:28:45 +0600

    some minor changes

--- src/main/java/com/konzerra/selim_server/domain/order/OrderController.java
+++ src/main/java/com/konzerra/selim_server/domain/order/OrderController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("selim/order")
+@RequestMapping("/orders")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
--- src/main/java/com/konzerra/selim_server/domain/order/impl/OrderMapperImpl.java
+++ src/main/java/com/konzerra/selim_server/domain/order/impl/OrderMapperImpl.java
@@ -6,11 +6,11 @@ import com.konzerra.selim_server.domain.order.dto.OrderDto;
 
 public class OrderMapperImpl implements OrderMapper {
     @Override
-    public Order mapToEntity(OrderDto dto) {
+    public Order mapToEntity(OrderDto orderDto) {
         Order order = new Order();
-        order.setCustomerName(dto.getCustomerName());
-        order.setCustomerPhone(dto.getCustomerPhone());
-        order.setMessage(dto.getMessage());
+        order.setCustomerName(orderDto.getCustomerName());
+        order.setCustomerPhone(orderDto.getCustomerPhone());
+        order.setMessage(orderDto.getMessage());
         return order;
     }
 }
