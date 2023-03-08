0111520c678fb5950c40d6646f4529acfc4c2f5b
invsblmad <aizatmadumarova04@gmail.com>
2023-03-08 16:51:46 +0600

    updated

--- src/main/java/com/konzerra/selim_server/domain/news/NewsController.java
+++ src/main/java/com/konzerra/selim_server/domain/news/NewsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/news")
 @RequiredArgsConstructor
-public class    NewsController {
+public class NewsController {
     private final NewsService newsService;
 
     @GetMapping
