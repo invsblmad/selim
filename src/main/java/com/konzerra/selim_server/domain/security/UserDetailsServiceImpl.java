93916d46e329e583ae5e5a7077f9b8ee6d7c0706
invsblmad <aizatmadumarova04@gmail.com>
2023-03-12 11:54:15 +0600

    created post and put queries for news

--- src/main/java/com/konzerra/selim_server/domain/security/UserDetailsServiceImpl.java
+++ src/main/java/com/konzerra/selim_server/domain/security/UserDetailsServiceImpl.java
@@ -3,7 +3,6 @@ package com.konzerra.selim_server.domain.security;
 import com.konzerra.selim_server.domain.user.User;
 import com.konzerra.selim_server.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -13,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException("User not found"));
         return new UserDetailsImpl(user);
--- src/main/java/com/konzerra/selim_server/domain/security/jwt/TokenService.java
+++ src/main/java/com/konzerra/selim_server/domain/security/jwt/TokenService.java
@@ -2,7 +2,9 @@ package com.konzerra.selim_server.domain.security.jwt;
 
 
 import com.konzerra.selim_server.domain.security.UserDetailsImpl;
+import com.konzerra.selim_server.domain.user.User;
 
 public interface TokenService {
     TokenResponse generateToken(UserDetailsImpl userDetails);
+    User getUserFromToken();
 }
